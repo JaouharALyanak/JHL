d87b56e6cb1292963aefd3570fc12931d3f67297
Franky <soufianecompaq@gmail.com>
2023-05-07 19:28:07 +0100

    add thanks route

---
+++ resources/views/thanks.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="description" content="Agence création site web au Maroc , Création Site Web Maroc, Référencement et Conception Graphique au Maroc, Création de site Web Design Unique , Entretient Gratuit ,  Création Site eCommerce, Création de Site Web eCommerce au Maroc Meilleur Qualité et Prix " />
+	<link rel="canonical" href="https://www.jhl.ma/" />
+    <meta property="og:locale" content="fr_FR" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Création Site Web Maroc, Création Site eCommerce et Marketing" />
+    <meta property="og:description" content="Agence création site web au Maroc meilleur rapport qualité prix, Création Site Web Maroc, Référencement et Conception Graphique  au Maroc, Création de site Web Design Unique , Entretient Gratuit ,  Création Site eCommerce, Création de Site Web eCommerce au Maroc Meilleur Qualité et Prix " />
+    <meta property="og:url" content="https://www.jhl.ma/" />
+    <meta property="og:site_name" content="Création Site Web Maroc, Création Site eCommerce et Marketing" />
+    <meta property="article:publisher" content="https://www.facebook.com/jhl.solution" />
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:title" content="Création Site Web Maroc, Création Site eCommerce et Marketing" />
+	<meta name="twitter:description" content="Agence création site web au Maroc meilleur rapport qualité prix, Création Site Web Maroc, Référencement et Conception Graphique  au Maroc, Création de site Web Design Unique , Entretient Gratuit ,  Création Site eCommerce, Création de Site Web eCommerce au Maroc Meilleur Qualité et Prix " />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/favicon.png') }}">
+    <title>Création site web au Maroc - Meilleur rapport qualité prix</title>
+
+    @vite([ 'resources/css/app.css', 'resources/js/app.js' ])
+</head>
+<body>
+    <div id="metodoviral-gradient">
+        <canvas id="gradient-canvas" data-js-darken-top data-transition-in></canvas>
+    </div>
+
+
+    <header class="mainheader">
+        <nav class="navbar navbar-expand-md">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img class="img-fluid" src="{{asset('assets/images/logo.svg') }}" alt="">
+                </a>
+                <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#pageMenu" aria-controls="pageMenu"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="pageMenu">
+                    <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+                        <li class="nav-item"> <a class="nav-link" href="#"> About Us </a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="#"> Services </a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="#"> Projects </a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="#"> Contact Us </a> </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main style="background-image: none;" id="mainContent">
+
+        <section class="aboutus sp-1 bg-shadow-1">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-md-6 cms-block order-md-1 order-2">
+                        <h2 class="title"> Nous vous remercions d'avoir pris contact avec</h2>
+                        <div class="cms-content">
+                            <p>
+                                Notre équipe a bien reçu votre message et nous sommes en train d'examiner les détails que vous avez fournis. Nous comprenons l'importance de votre demande et nous nous engageons à vous répondre dans les plus brefs délais.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-5 ms-auto order-md-2 order-1 mb-md-0 mb-4">
+                        <img class="img-fluid lazyload" loading="lazy" width="500" height="460" data-src="{{asset('assets/images/working.png') }}" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <footer class="footer">
+        <div class="container text-center">
+            <p class="py-3 m-0"> All Right reserved 2023</p>
+        </div>
+    </footer>
+
+    {{-- <div id='metodoviral-gradient-diagonal'><canvas id='gradient-canvas-diagonal'
+        data-js-darken-top data-transition-in></canvas>
+    </div> --}}
+
+    {{-- <script src="{{ asset('assets/js/jquery-3.6.4.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/slick.min.js') }}"></script>
+    <script src="{{ asset('assets/js/gradientbgmv.js') }}"></script>
+    <script src="{{ asset('assets/js/typed.min.js') }}"></script>
+    <script src="{{ asset('assets/js/script.js') }}"></script> --}}
+
+    @if ($errors->any())
+    <script>
+        $(document).ready(function () {
+            $(window).scrollTop( $('#formContact').offset().top - 40);
+        })
+    </script>
+    @endif
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'index');
+Route::view('/', 'index')->name('home');
 Route::post('contact', [ ContactController::class ,'send'])->name('contact.send');
+Route::get('/merci', function () {
+    if( request()->session()->has('msg_thanks') ) return view('thanks');
 
+    return redirect()->route('home');
+})->name('msg.thanks');
